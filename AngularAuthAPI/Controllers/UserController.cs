2c7c933791aa2c1d6f3632d20c5fce799f0d6d5e
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-05-29 02:44:31 +0530

    Add project files.

---
+++ AngularAuthAPI/Controllers/UserController.cs
@@ -0,0 +1,138 @@
+﻿using AngularAuthAPI.Context;
+using AngularAuthAPI.Helpers;
+using AngularAuthAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AngularAuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        public UserController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        [HttpPost("Authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if (userObj == null)
+            {
+                return BadRequest();
+            }
+
+            var user = await _appDbContext.Users.FirstOrDefaultAsync(u => u.UserName == userObj.UserName);
+            if (user == null)
+            {
+                return NotFound(new { message = "User not found!" });
+            }
+
+            if (!PasswordHasher.VerifyPassword(userObj.Password, user.Password))
+                return BadRequest(new { Message = "Password is incorrect!" });
+
+            user.Token = CreateJwtToken(user);
+
+            return Ok(new
+            {
+                Token = user.Token,
+                Message = "Login success!"
+            }); ;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] User userObj)
+        {
+            if (userObj == null)
+            {
+                return BadRequest();
+            }
+            // check username
+            if (await CheckUserNameExistAsync(userObj.UserName))
+                return BadRequest(new { Message = "UserName Already Exist!" });
+
+            // check email
+            if (await CheckEmailExistAsync(userObj.Email))
+                return BadRequest(new { Message = "Email Already Exist!" });
+
+            // check password strength
+            var pass = CheckPasswordStrength(userObj.Password);
+            if (!string.IsNullOrEmpty(pass))
+            {
+                return BadRequest(new { Messsage = pass });
+            }
+
+
+            userObj.Password = PasswordHasher.HashPassword(userObj.Password);
+            userObj.Role = "Client";
+            userObj.Token = "";
+
+            await _appDbContext.Users.AddAsync(userObj);
+            await _appDbContext.SaveChangesAsync();
+            return Ok(new { message = "User registerd!" });
+        }
+
+        private async Task<bool> CheckUserNameExistAsync(string userName)
+        {
+            return await _appDbContext.Users.AnyAsync(u => u.UserName == userName);
+        }
+        private async Task<bool> CheckEmailExistAsync(string email)
+        {
+            return await _appDbContext.Users.AnyAsync(u => u.Email == email);
+        }
+        private string CheckPasswordStrength(string password)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (password.Length < 8)
+                sb.Append("Minimum password length should be 8" + Environment.NewLine);
+
+            if (!(Regex.IsMatch(password, "[a-z]") && Regex.IsMatch(password, "[A-Z]") && Regex.IsMatch(password, "[0-9]")))
+                sb.Append("Password should be Alphanumeric" + Environment.NewLine);
+
+            if (!Regex.IsMatch(password, "[`,~,!,@,#,$,%,^,&,*,(,),_,-,+,=,{,[,},},|,\\,:,;,\",',<,,,>,.,?,/]"))
+                sb.Append("Password should contain special character" + Environment.NewLine);
+            return sb.ToString();
+
+        }
+
+        private string CreateJwtToken(User user)
+        {
+
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("veryveryveryveryveryveryverververververyveryverysceret.....");
+            var identity = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Role, user.Role),
+                new Claim(ClaimTypes.Name,$"{user.FirstName} {user.LastName}")
+
+            });
+
+            var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = identity,
+                Expires = DateTime.Now.AddSeconds(10),
+                SigningCredentials = credentials
+            };
+
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+            return jwtTokenHandler.WriteToken(token);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<User>> GetAllUsers()
+        {
+            return Ok(await _appDbContext.Users.ToListAsync());
+        }
+    }
+}
